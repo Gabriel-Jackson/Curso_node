763f7a372739f529b4eec417f3d9d1eb9a30461b
Gabriel-Jackson <gabrielchrists@gmail.com>
2019-12-16 03:31:50 -0300

    Arquivo raiz da aplicação

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
-var express = require('express');
-var app = express();
+var app = require ("./config/server")
 
-var msg = require('./mod_teste');
-app.set('view engine','ejs');
-
-app.get('/', function(req, res){
-    res.render("home/index");
-});
-
-app.get('/add_noticia', function(req, res){
-    res.render("admin/form_add_noticia");
-});
-
-app.get('/noticias', function(req, res){
-    res.render("noticias/noticias");
-});
 app.listen(3000, function(){
-    console.log(msg);
+    console.log("Servidor On");
 });
